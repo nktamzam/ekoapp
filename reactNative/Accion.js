4c4749257440df31f278056d2cc2f51c46618b8e
nktamzam <eneko.tamayo@ikasle.aeg.eus>
2019-11-04 11:44:07 +0100

    comentarios añadidos

--- reactNative/Accion.js
+++ reactNative/Accion.js
@@ -8,13 +8,17 @@ import Cabecera from "./src/components/cabecera";
 class Accion extends Component {
   static navigationOptions = {};
   componentDidMount() {
+    // recogemos el id enviado con la navegación
     const id = this.props.navigation.getParam("id");
+    // llamamos a la Apirest para que consulte
     this.props.getAccion(id);
   }
   render() {
+    // destructuring: extraemos variables accion, loadingInfo the this props
     const { accion, loadingInfo } = this.props;
     if (loadingInfo) return <Text>Cargando...</Text>;
 
+    // destructuring: extraemos las variables del objeto accion (este )
     const { titulo, texto, energia, residuos, dificultad } = accion;
 
     return (
@@ -30,15 +34,18 @@ class Accion extends Component {
   }
 }
 
+// el state (accion y loadinginfo) lo convertimos en props
 const mapStateToProps = ({ accion, loadingInfo }) => ({
   accion,
   loadingInfo
 });
 
+// la accion la convertimos en props
 const mapDispatchToProps = {
   getAccion
 };
 
+// exportamos con el state y la acción cómo props
 export default connect(
   mapStateToProps,
   mapDispatchToProps
