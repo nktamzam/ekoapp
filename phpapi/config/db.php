13793fe20831c768016f238512e8a92d303a386c
nktamzam <eneko.tamayo@ikasle.aeg.eus>
2019-11-04 14:36:59 +0100

    hide pass

---
+++ phpapi/config/db.php
@@ -0,0 +1,32 @@
+<?php
+include 'pass.php';
+
+class Database{
+    
+    private $password;
+    public function __construct($dbpass) {
+      $this->password=$dbpass;
+    } 
+    
+    // specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "ekoappdb";
+    private $username = "ekoappusr";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
